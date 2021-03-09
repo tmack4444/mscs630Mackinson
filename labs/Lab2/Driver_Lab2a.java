635ca77d7333e1c1a8ea0067a8b021c4bbfa5824
tmack4444 <>
2021-03-09 11:29:10 -0500

    Don't know why this didn't commit either. I finished A last week

--- labs/Lab2/Driver_Lab2a.java
+++ labs/Lab2/Driver_Lab2a.java
@@ -1,27 +1,34 @@
 import java.util.Scanner;
 
-public class Driver_lab2a{
+class Driver_Lab2a{
   public static void main(String [] args){
     Scanner in = new Scanner(System.in);
     while(in.hasNext()){
       long a = in.nextInt();
       long b = in.nextInt();
-      euclidAlg(a, b);
+      if(a < b){
+        long temp = a;
+        a = b;
+        b = temp;
+      }
+      System.out.println(euclidAlg(a, b));
     }
   }
+
   public static long euclidAlg(long a, long b){
     // a should always be greater than b, but just in case
-    if(a < b){
-      long temp = a;
-      a = b;
-      b = temp;
-    }
     long remainder;
     long quotient = 1;
     do{
       remainder = a % b;
+      if(remainder == 0){
+        return b;
+      } else {
+        a = b;
+        b = remainder;
+      }
     }while(remainder != 0);
-
+  return b;
   }
 
 
