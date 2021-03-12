346b022d60511400dc8f880bf0a85b54fd8c7b34
tmack4444 <>
2021-03-12 12:55:29 -0500

    I had an idea on how to solve this problem, using stacks to keep track of different stuff but that got very confusing very quick. I think if I use arraylists and arrays I can keep track of terms, and rather than reduce immediatley, I can substitute arrays instead of individual terms

--- labs/Lab2/Driver_lab2b.java
+++ labs/Lab2/Driver_lab2b.java
@@ -1,4 +1,5 @@
-
+import java.util.Stack;
+import java.util.Scanner;
 
 public class Driver_lab2b{
 
@@ -16,28 +17,55 @@ public class Driver_lab2b{
     }
   }
 
-  public static long[] euclidAlgExt(long a, long b) {
+  public static long euclidAlgExt(long a, long b) {
+      Stack<Long> qs = new Stack<Long>();    //qs is stack of quotients
+      Stack<Long> ms = new Stack<Long>();    //ms is stack of multipliers (What number we multiply previous remainder by)
+      Stack<Long> ds = new Stack<Long>();    //ds is stack of (potential) divisors
+      Stack<Long> rs = new Stack<Long>();    //rs is stack of remainders
       long remainder = -1;
-      long rLast;
       long aSave = a;
       long bSave = b;
-      long d = 1;
+      long u = 0;
+      long v = 0;
       do{
-        rLast = remainder;
         remainder = a % b;
-        if(remainder == 0){
-          d = b;
-        } else {
+        if(remainder != 0){
+          rs.push(remainder);
+          qs.push(a);
+          ms.push(a / b);
+          ds.push(b);
           a = b;
           b = remainder;
         }
       }while(remainder != 0);
+      //We don't care about the last line of the equation, we need to start at line n-1
+        System.out.println("rs.peek(): " + rs.peek());
+        System.out.println("qs.peek(): " + qs.peek());
+        System.out.println("ds.peek(): " + ds.peek());
+      //declare the vars that we will substitute to find our final u v
+      long q = qs.peek();
+      long m = ms.peek();
+      long d = ds.peek();
+      long r = 0;
+      while(!qs.empty() && !ms.empty() && !ds.empty() && !rs.empty()){
+          r = rs.pop();
+            System.out.println("r: " + r);
+            System.out.println("q: " + q);
+            System.out.println("d: " + d);
+          if(d == r){
+            d = qs.pop() - ds.pop() * ms.pop();
+            System.out.println("new d: " + d);
+          } else if(q == r){
+            q = qs.pop() - ds.pop() * ms.pop();
+            System.out.println("new q: " + q);
+          }
+      }
+      u = d;
+      v = q;
+      System.out.println(u);
+      System.out.println(v);
 
-    while(aSave * remainder + bSave * rLast){
-
-    }
-
-    return [d, rLast, remainder];
+    return remainder;
 
   }
 
