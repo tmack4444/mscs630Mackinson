37096174f6b26998244e3c57759171acad170b65
tmack4444 <>
2021-03-13 16:21:39 -0500

    Just starting part b now, making sure I get the input

---
+++ labs/Lab3/Driver_lab3b.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Driver_lab3b {
+
+
+  public static void main(String [] args){
+    Scanner in = new Scanner(System.in);
+    String pad = in.nextLine();
+    String input = in.nextLine();
+
+
+
+  }
+}
