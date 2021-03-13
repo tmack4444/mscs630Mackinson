81939f2730c2e38b2f4eab5adf9f4107085b9926
tmack4444 <>
2021-03-13 15:19:01 -0500

    Fixed the filenames so they have lowercase l as you said in the doc. Also formatted the output for 2b so it matches yours exactly. Should be done now, going to submit on ilearn then back to lab 3

--- labs/Lab2/Driver_lab2b.java
+++ labs/Lab2/Driver_lab2b.java
@@ -5,14 +5,16 @@ public class Driver_lab2b{
 
   public static void main(String args[]){
     Scanner in = new Scanner(System.in);
+    String output = "";
     while(in.hasNext()){
       long a = in.nextInt();
       long b = in.nextInt();
       long[] result = euclidAlgExt(a, b);
-      System.out.println(result[0]);
-      System.out.println(result[1]);
-      System.out.println(result[2]);
+      output += (result[0] + " " + result[1] + " " + result[2] + " \n");
     }
+    output = output.trim();
+    output += " ";
+    System.out.print(output);
   }
   /*
    * Ok, I took a ton of time to get here but just to be sure I don't forget...
@@ -35,7 +37,9 @@ public class Driver_lab2b{
       long saveA = a;
       long saveB = b;
       long result[] = new long[3];
+      boolean flip = false;
       if(a < b){
+        flip = true;
         temp = a;
         a = b;
         b = temp;
@@ -59,6 +63,11 @@ public class Driver_lab2b{
           q = b;
         }
       }while(remainder != 0);
+      if(flip){
+        temp = x;
+        x = y;
+        y = temp;
+      }
       result[0] = b;
       result[1] = x;
       result[2] = y;
