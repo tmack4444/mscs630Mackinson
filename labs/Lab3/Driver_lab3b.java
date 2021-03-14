85f7e6be0d215b5ffde70fc419ac29e4c0a2ab04
tmack4444 <>
2021-03-14 13:56:26 -0400

    I think I understand this one a lot better. So basically, get the remainder of string length / 16 and add that much padding to the input string. Then count how many 16 length arrays we can make and call a function with 16 length substrings of input to make them

--- labs/Lab3/Driver_lab3b.java
+++ labs/Lab3/Driver_lab3b.java
@@ -1,14 +1,38 @@
 import java.util.Scanner;
+import java.lang.Byte;
 
 public class Driver_lab3b {
 
 
   public static void main(String [] args){
     Scanner in = new Scanner(System.in);
-    String pad = in.nextLine();
-    String input = in.nextLine();
-
-
-
+    String pad = in.next();
+    String P = in.nextLine();
+    int numPad = P.length() % 16;
+    for(int i = 0; i < numPad; i++){
+      P += pad;
+    }
+    int numArray = Math.max(1, P.length() / 16);
+    int result[][] = new int[4][4*numArray];
+    System.out.println("P.length: " + P.length());
+    System.out.println("numPad: " + numPad);
+    System.out.println("numArray " + numArray);
+    for(int i = 0; i < numArray; i++) {
+      String p = P.substring(i*16, (i*16 + 16));
+      getHexMatP(p);
+    }
   }
-}
+
+  public static int[][] getHexMatP(String p){
+    int[][] result = new int[4][4];
+    for(int i = 0; i < 4; i++){
+      for(int j = 0; j < 4; j++){
+        byte currVal = Byte.parseByte(p.charAt( (i+1 * j) ) + "" );
+        result[i][j] = currVal;
+        System.out.print(result[i][j] + " ");
+      }
+      System.out.println();
+    }
+    return result;
+    }
+  }
