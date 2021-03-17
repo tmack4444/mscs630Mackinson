28cd532f9fe8c588bac859ff1e889ff8588526c5
tmack4444 <>
2021-03-17 18:45:38 -0400

    Working on adding the driver. Having a slight issue with getting the roundKeysHex method to run, but I just want to submit the starter driver

--- labs/Lab4/Driver_lab4.java
+++ labs/Lab4/Driver_lab4.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 
 public class Driver_lab4 {
 
  public static void main(String [] args){
-   
+   Scanner in = new Scanner(System.in);
+   String input = in.nextLine();
+   String[] Keys = roundKeysHex(in);
+
  }
 
 }
