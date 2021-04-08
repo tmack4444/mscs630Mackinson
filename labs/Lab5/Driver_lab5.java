5906ca2f596fef8883ddac6592865abad8835de7
tmack4444 <>
2021-04-08 11:13:37 -0400

    Code def doesn't work. Getting an array out of bounds error so I have some more debugging to do

---
+++ labs/Lab5/Driver_lab5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Driver_lab5 {
+
+ public static void main(String [] args){
+   Scanner in = new Scanner(System.in);
+   String input = in.nextLine();
+   String plaintext = in.nextLine();
+   AESCipher keygen = new AESCipher();
+   String[][] Keys = keygen.AESSeq(input, plaintext);
+   String output = "";
+   for(int i = 0; i < 4; i++){
+     for(int j = 0; j < 4; j++){
+       output += Keys[i][j].toUpperCase() + "\n";
+     }
+   }
+   output = output.trim();
+   System.out.println(output);
+ }
+
+}
