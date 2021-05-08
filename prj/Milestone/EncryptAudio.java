e92da0467417ea6ee6b2c5b1b8a9c8af420c84d8
tmack4444 <>
2021-05-08 16:59:39 -0400

    It's been a long saturday, I think I'm pretty much finished with the research paper, and now this code is much closer to being done. It takes longer to run then I would like, however that might be what I have to deal wtih for this implementation. My AES encryption algorithm isn't exactly the most efficient, I could have made some big changes to it, but I feel that would have taken too long and put the rest of the project at risk of not being finished in time. I know that there is still plenty of time in the project to work on this, so I'd like something that works, so I have time to improve it before the due date

--- prj/Milestone/EncryptAudio.java
+++ prj/Milestone/EncryptAudio.java
@@ -11,8 +11,9 @@ public class EncryptAudio {
 
     public static void main(String[] args) throws IOException {
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter File Name: ");
-      String sourceFile = input.nextLine();
+    //  System.out.print("Enter File Name: ");
+      //String sourceFile = input.nextLine();
+      String sourceFile = "plaintext.mp3";
       /*System.out.print("Enter Encryption Key: ");
       String key = input.nextLine();
       if(key.length() > 32){
@@ -23,32 +24,51 @@ public class EncryptAudio {
       key = Integer.toHexString
       */
       String key = keyGen();
-      System.out.println(key);
+      //System.out.println(key);
       File audio = new File(sourceFile);
       InputStream is = new FileInputStream(audio);
       byte[] buffer = new byte[(int) audio.length()];
-      System.out.println("Now reading file");
+      //System.out.println("Now reading file");
       is.read(buffer);
       is.close();
       String bufferToHexArray = "";
-      System.out.println("Now loading file to hexString");
+      //System.out.println("Now loading file to hexString");
       final long startTime = System.currentTimeMillis();
       for(int i = 0; i < buffer.length; i++){
         String temp = Integer.toHexString(Byte.toUnsignedInt(buffer[i]));
         if(temp.length() == 1)
           bufferToHexArray += "0";
         bufferToHexArray += temp;
+        /* Debugging code simply to get the amount of time it takes to convert every 100000 bytes to hex string
         if(i % 100000 == 0){
           System.out.println(i);
           System.out.println((System.currentTimeMillis() - startTime) / 1000);
         }
+        */
       }
-      System.out.println("Now loading hexString to array of hexStrings");
-      String[] plaintext = new String[bufferToHexArray.length()/32];
-      for(int j = 0; j < plaintext.length; j++){
-        plaintext[j] = bufferToHexArray.substring(j+1 , j+1 * 32);
+      //System.out.println("Now loading hexString to array of hexStrings");
+      String[][][] Keys = new String[bufferToHexArray.length()/32][4][4];
+      AESCipher encrypt = new AESCipher();
+      for(int j = 0; j < Keys.length; j++){
+        Keys[j] = encrypt.AESSeq(key, bufferToHexArray.substring(j*32, j*32 + 32));
       }
-      System.out.println("Done!");
+      int iterator = 0;
+      String[] output = new String[Keys.length];
+      for(int i = 0; i < Keys.length; i++){
+        for(int r = 0; r < Keys[i].length; r++){
+          for(int c = 0; c < Keys[i][r].length; c++){
+            if(Keys[i][r][c].length() == 1){
+              output[iterator] += "0";
+            }
+            output[iterator] += Keys[i][r][c];
+          }
+        }
+        System.out.println(output[iterator]);
+        iterator++;
+      }
+
+      //System.out.println("Done!");
+
       //Encrypt the file
 
       //Instead of outputing the input stream, convert it to hex, and
